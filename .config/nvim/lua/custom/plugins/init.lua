9f9ac90c830e88ce0d3803909ad29834fea8f740
Dan Amoroso <dan@deadend.dev>
2025-10-28 11:49:53 +0700

    nvim: try mini.animate for cursor animation

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -52,4 +52,39 @@ return {
     ft = { 'clojure', 'fennel', 'lisp' },
   },
   { 'adelarsq/vim-matchit' },
+  -- Cursor animation for better visibility
+  {
+    'echasnovski/mini.animate',
+    event = 'VeryLazy',
+    config = function()
+      local animate = require 'mini.animate'
+      animate.setup {
+        -- Cursor path animation
+        cursor = {
+          enable = true,
+          timing = animate.gen_timing.linear { duration = 80, unit = 'total' },
+          path = animate.gen_path.line {
+            predicate = function()
+              return true -- Animate all cursor movements
+            end,
+          },
+        },
+        -- Scroll animation (optional, can disable if you don't like it)
+        scroll = {
+          enable = false,
+        },
+        -- Resize animation
+        resize = {
+          enable = false,
+        },
+        -- Open/close animation
+        open = {
+          enable = false,
+        },
+        close = {
+          enable = false,
+        },
+      }
+    end,
+  },
 }
