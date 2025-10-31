c7505958d6122775f46c86a1077366ff0c955641
Dan Amoroso <dan@deadend.dev>
2025-10-31 15:32:04 +0700

    fix: autocmds for markdown cursor out of buffer error

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -10,6 +10,13 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   callback = function()
     local pos = vim.api.nvim_win_get_cursor(0) -- Save current cursor position
     vim.cmd 'normal! ggVGgq' -- Reformat whole buffer
-    vim.api.nvim_win_set_cursor(0, pos) -- Restore cursor position
+    
+    -- Safely restore cursor position, clamping to valid range
+    local line_count = vim.api.nvim_buf_line_count(0)
+    local safe_row = math.min(pos[1], line_count)
+    local line_length = #vim.api.nvim_buf_get_lines(0, safe_row - 1, safe_row, false)[1]
+    local safe_col = math.min(pos[2], line_length)
+    
+    vim.api.nvim_win_set_cursor(0, { safe_row, safe_col })
   end,
 })
